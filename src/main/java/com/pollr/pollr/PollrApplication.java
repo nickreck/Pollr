77425f6c1b6433bdd167ed4e3184e6dcc14fe0ba
Nick Reck <nicholasreck@outlook.com>
2024-05-04 19:09:33 -0400

    Add generated spring project

---
+++ src/main/java/com/pollr/pollr/PollrApplication.java
@@ -0,0 +1,13 @@
+package com.pollr.pollr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pollr/pollr/PollrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pollr.pollr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
